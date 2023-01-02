d65390c215a6e8702a67663118e11f85f71d1f57
Ihor Zakhozhyi <ihorzak@gmail.com>
2023-01-02 21:08:17 +0200

    Update year in copyright statements

--- library/src/main/kotlin/ua/pp/ihorzak/aktormailbox/Mailbox.kt
+++ library/src/main/kotlin/ua/pp/ihorzak/aktormailbox/Mailbox.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Ihor Zakhozhyi
+ * Copyright 2023 Ihor Zakhozhyi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package ua.pp.ihorzak.aktormailbox
 
 /**
- * // TODO Describe
+ *
  */
 public interface Mailbox<I, O> {
     public val isEmpty: Boolean
